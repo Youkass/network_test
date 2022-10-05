530f24ca2a0d6f7f3668378cc140bdc42b212bed
Youkass <youribougre@gmail.com>
2022-10-05 17:52:03 +0200

    first push

--- srcs/client.c
+++ srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: yobougre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 13:37:10 by yobougre          #+#    #+#             */
-/*   Updated: 2022/10/05 11:45:19 by yobougre         ###   ########.fr       */
+/*   Updated: 2022/10/05 17:49:56 by yobougre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,27 @@ int	main(void)
 {
 	t_server	client;
 	t_user		user;
-	char		msg[90];
-
+	char		*msg;
+	char		fix_msg[90];
+	int			size;
+	
+	(void)user;
 	client.socket = socket(AF_INET, SOCK_STREAM, 0);
-	client.addr.sin_addr.s_addr = inet_addr("10.12.7.1");
+	client.addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	client.addr.sin_family = AF_INET;
 	client.addr.sin_port = htons(30000);
 	connect(client.socket, (const struct sockaddr *)&(client.addr), sizeof(client.addr));
 	printf("connecté\n");
 /*=============================================================================*/
+	recv(client.socket, &size, sizeof(size), 0);
+	printf("size : %d\n", size);
+	
+	recv(client.socket, &fix_msg, sizeof(fix_msg), 0);
+	printf("%s\n", fix_msg);
+
+	msg = malloc(sizeof(char) * (strlen(fix_msg) + 1));
 	recv(client.socket, &msg, sizeof(msg), 0);
-	printf("%s", msg);
+	printf("%s\n", msg);
 	close(client.socket);
 
 	return (0);
--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: yobougre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 13:31:12 by yobougre          #+#    #+#             */
-/*   Updated: 2022/10/05 11:57:51 by yobougre         ###   ########.fr       */
+/*   Updated: 2022/10/05 17:51:58 by yobougre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,14 @@ int	main(void)
 	socklen_t	csize;
 	t_user		user = {.name = "Arthur", .age = 42};
 	char		msg[]="Salut Valentin, t'es beau aujourd'hui\n";
+	char		fix_msg[]="Salut Valentin, t'es beau aujourd'hui\n";
+	int			size;
 
 	server.socket = socket(AF_INET, SOCK_STREAM, 0);
-	server.addr.sin_addr.s_addr = inet_addr("10.12.7.1");
+	server.addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	server.addr.sin_family = AF_INET;
 	server.addr.sin_port = htons(30000);
+	(void)user;
 /*===========================================================================*/
 	bind(server.socket, (const struct sockaddr *)&(server.addr), sizeof(server.addr));
 	printf("bind : %d\n", server.socket);
@@ -31,11 +34,15 @@ int	main(void)
 	listen(server.socket, 5);
 	printf("listen\n");
 /*===========================================================================*/
-	csize = sizeof(client.addr);
+	size = strlen(msg);
+	printf("size : %d\n", size);
+	csize = sizeof(client.addr)fix_;
 	client.socket = accept(server.socket, (struct sockaddr *)&(client.addr), &csize);
 	printf("accepted\n");
 /*===========================================================================*/
+	send(client.socket, &size, sizeof(size), 0);
 	send(client.socket, &msg, sizeof(msg), 0);
+	send(client.socket, &fix_msg, sizeof(fix_msg), 0);
 	close(client.socket);
 	close(server.socket);
 	return (0);
